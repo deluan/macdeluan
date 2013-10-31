4b78f006b5cb7fcebbd97e25e7728c008b5fc3fe
Deluan <deluan@thoughtworks.com>
2013-10-31 15:06:31 -0400

    Installing vagrant key

--- .profile.d/aliases.sh
+++
@@ -1,8 +0,0 @@
-alias ll='ls -la'
-alias webserver='python -m SimpleHTTPServer' 
-alias gradlew='./gradlew -Dskip.android'
-alias st='stree'
-
-export PATH=~/bin:/usr/local/bin:$PATH
-export EDITOR='subl -w'
-export CLICOLOR=1

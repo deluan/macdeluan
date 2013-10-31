f1bf123f11b9cfb7e5b15e038be8bcead4e9576a
Deluan <deluan@thoughtworks.com>
2013-10-31 17:25:47 -0400

    Some environment variables adjusts

--- .profile.d/00-env.sh
+++ .profile.d/00-env.sh
@@ -1,6 +1,5 @@
 alias ll='ls -la'
 alias webserver='python -m SimpleHTTPServer' 
-alias gradlew='./gradlew -Dskip.android'
 alias st='stree'
 
 export PATH=~/bin:/usr/local/bin:$PATH
@@ -8,6 +7,6 @@ export EDITOR='subl -w'
 export CLICOLOR=1
 
 export JAVA_HOME=$(/usr/libexec/java_home)
-export ANDROID_HOME=/Users/Thoughtworker/Development/adt-bundle-mac-x86_64-20130219/sdk
+export ANDROID_HOME=$HOME/Development/adt-bundle-mac-x86_64-20130219/sdk
 export GRAILS_HOME=/Applications/grails/grails-2.3.0
 export GRIFFON_HOME=/Applications/griffon/griffon-1.4.0

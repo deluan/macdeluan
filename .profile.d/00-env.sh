eed0bcce1b719aae1e34f2cbdd85f2f76c60f5dd
Deluan <deluan@thoughtworks.com>
2013-11-01 11:22:18 -0400

    setting java memory options

--- .profile.d/00-env.sh
+++ .profile.d/00-env.sh
@@ -6,6 +6,7 @@ export PATH=~/bin:/usr/local/bin:$PATH
 export EDITOR='subl -w'
 export CLICOLOR=1
 
+export JAVA_OPTS="-Xms256m -Xmx1024m -XX:PermSize=512m -XX:MaxPermSize=512m"
 export JAVA_HOME=$(/usr/libexec/java_home)
 export ANDROID_HOME=$HOME/Development/adt-bundle-mac-x86_64-20130219/sdk
 export GRAILS_HOME=/Applications/grails/grails-2.3.0

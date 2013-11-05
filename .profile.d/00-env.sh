091a65349a8fad3ef8623b2aeb4beda078afaf28
Deluan <deluan@thoughtworks.com>
2013-11-05 13:50:14 -0500

    Auto detect latest grails & griffon version installed

--- .profile.d/00-env.sh
+++ .profile.d/00-env.sh
@@ -9,5 +9,5 @@ export CLICOLOR=1
 export JAVA_OPTS="-Xms256m -Xmx1024m -XX:PermSize=512m -XX:MaxPermSize=512m"
 export JAVA_HOME=$(/usr/libexec/java_home)
 export ANDROID_HOME=$HOME/Development/adt-bundle-mac-x86_64-20130219/sdk
-export GRAILS_HOME=/Applications/grails/grails-2.3.0
-export GRIFFON_HOME=/Applications/griffon/griffon-1.4.0
+export GRAILS_HOME=/Applications/grails/`ls  /Applications/grails/ | tail -n 1`
+export GRIFFON_HOME=/Applications/griffon/`ls  /Applications/griffon/ | tail -n 1`

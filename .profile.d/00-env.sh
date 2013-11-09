2b003b59a70f0d0d73846e7b128cbeb31bca8b90
Deluan <deluan@thoughtworks.com>
2013-11-09 10:56:44 -0500

    Changes in the JAVA memory options

--- .profile.d/00-env.sh
+++ .profile.d/00-env.sh
@@ -6,7 +6,8 @@ export PATH=~/bin:/usr/local/bin:$PATH
 export EDITOR=vi
 export CLICOLOR=1
 
-export JAVA_OPTS="-Xms256m -Xmx1024m -XX:PermSize=512m -XX:MaxPermSize=512m"
+export JAVA_OPTS="-Xms256m -Xmx1024m -XX:PermSize=128m -XX:MaxPermSize=256m"
+export MAVEN_OPTS="-Xmx512m -XX:MaxPermSize=256m"
 export JAVA_HOME=$(/usr/libexec/java_home)
 export ANDROID_HOME=$HOME/Development/adt-bundle-mac-x86_64-20130219/sdk
 export GRAILS_HOME=/Applications/grails/`ls  /Applications/grails/ | tail -n 1`

c07ac37cdda37c717656d7bfad134f6458bf2006
Deluan <deluan@thoughtworks.com>
2015-09-30 12:00:42 -0400

    function to add public key in a remote server

--- .profile.d/10-ssh_agent.sh
+++ .profile.d/10-ssh_agent.sh
@@ -1,6 +1,6 @@
 # Set up ssh-agent
 SSH_ENV="$HOME/.ssh/environment"
- 
+
 function start_agent {
     echo "Initializing new SSH agent..."
     /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
@@ -10,7 +10,7 @@ function start_agent {
     # /usr/bin/ssh-add;
     /usr/bin/ssh-add ~/.ssh/vagrant_rsa
 }
- 
+
 # Source SSH settings, if applicable
 if [ -f "${SSH_ENV}" ]; then
     . "${SSH_ENV}" > /dev/null
@@ -20,3 +20,7 @@ if [ -f "${SSH_ENV}" ]; then
 else
     start_agent;
 fi
+
+ssh_install_key() {
+  cat ~/.ssh/id_rsa.pub | ssh $1 "mkdir -p ~/.ssh && cat >>  ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys"
+}

b7ac7decb665ef9b3bf896a3a1afcc05247dd028
Deluan <deluan@thoughtworks.com>
2015-10-21 17:49:46 -0400

    Using brew android-sdk

--- .profile.d/00-env.sh
+++ .profile.d/00-env.sh
@@ -10,6 +10,6 @@ export POSTGRES_APP=/Applications/Postgres93.app/Contents/MacOS
 export JAVA_OPTS="-Xms256m -Xmx1024m -XX:PermSize=128m -XX:MaxPermSize=256m"
 export MAVEN_OPTS="-Xmx512m -XX:MaxPermSize=256m"
 export JAVA_HOME=$(/usr/libexec/java_home)
-export ANDROID_HOME=$HOME/Development/adt-bundle-mac-x86_64-20130219/sdk
+export ANDROID_HOME=/usr/local/opt/android-sdk
 
 export PATH=~/bin:/usr/local/bin:$POSTGRES_APP/bin:$PATH

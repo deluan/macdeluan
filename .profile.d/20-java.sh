dba0be185423f4b2ddf84482c5f5845addcac17b
Deluan <dquintao@adparlor.com>
2016-02-02 13:50:59 -0500

    Option to set default jdk version in ~/.jdkversion

--- .profile.d/20-java.sh
+++ .profile.d/20-java.sh
@@ -1,3 +1,5 @@
 alias jdk6='export JAVA_HOME=$(/usr/libexec/java_home -v 1.6)'
 alias jdk7='export JAVA_HOME=$(/usr/libexec/java_home -v 1.7)'
 alias jdk8='export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)'
+
+[ -f ~/.jdkversion ] && . ~/.jdkversion

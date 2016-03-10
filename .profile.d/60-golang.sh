aefa041fa9472e51ead5cea9b5cb93e11ac9d3d2
Deluan <dquintao@adparlor.com>
2016-03-10 14:34:23 -0500

    gocd cmd, for fast fuzzy chdir

--- .profile.d/60-golang.sh
+++ .profile.d/60-golang.sh
@@ -1,2 +1,9 @@
-export GOPATH=$HOME/Development/gowork
+export GOPATH=$HOME/Development/go
 export PATH=$PATH:/usr/local/opt/go/libexec/bin:$GOPATH/bin
+
+gocd() {
+  if [ $1 ]; then
+    PARAM="-q $1 -1"
+  fi
+  cd `find $GOPATH -type d | fzf $PARAM`
+}

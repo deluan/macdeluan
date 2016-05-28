cb61e604a47b5fb143d1de74a4e25ec4d77183da
Deluan <dquintao@adparlor.com>
2016-05-27 20:22:10 -0400

    GoLang fixes

--- .profile.d/60-golang.sh
+++ .profile.d/60-golang.sh
@@ -1,9 +1,26 @@
 export GOPATH=$HOME/Development/go
 export PATH=$PATH:/usr/local/opt/go/libexec/bin:$GOPATH/bin
+export GO15VENDOREXPERIMENT=1
 
-gocd() {
+gcd() {
   if [ $1 ]; then
     PARAM="-q $1 -1"
   fi
   cd `find $GOPATH -type d | fzf $PARAM`
+}
+
+
+gfmt() {
+  gofmtcmd=`which goimports || echo "gofmt"`
+
+  gofiles=$(git diff --name-only --diff-filter=ACM | grep '.go$')
+  [ -z "$gofiles" ] && return
+
+  unformatted=`$gofmtcmd -l $gofiles`
+  [ -z "$unformatted" ] && return
+
+  for f in $unformatted; do
+      $gofmtcmd -w -l "$f"
+      gofmt -s -w -l "$f"
+  done
 }

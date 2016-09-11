a3a222edf4bd2e653e2e6622bca9158345a23230
Deluan <dquintao@adparlor.com>
2016-09-11 13:24:33 -0400

    Unsetting temporary function

--- .profile.d/10-ssh_agent.sh
+++ .profile.d/10-ssh_agent.sh
@@ -1,7 +1,7 @@
 # Set up ssh-agent
 SSH_ENV="$HOME/.ssh/environment"
 
-function start_agent {
+function __start_agent {
     echo "Initializing new SSH agent..."
     /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
     echo succeeded
@@ -15,11 +15,13 @@ function start_agent {
 if [ -f "${SSH_ENV}" ]; then
     . "${SSH_ENV}" > /dev/null
     ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
-        start_agent;
+        __start_agent;
     }
 else
-    start_agent;
+    __start_agent;
 fi
+
+unset __start_agent # to avoid overriding the settings accidentally 
 
 ssh_install_key() {
   cat ~/.ssh/id_rsa.pub | ssh $1 "mkdir -p ~/.ssh && cat >>  ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys"

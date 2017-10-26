bc9c3c9e378cdd6719255f874fe473d6de00e2e5
Deluan^ <deluan@deluan.com>
2017-10-26 11:32:42 -0400

    Removes check for Git's user config

--- .profile.d/90-git_config.sh
+++
@@ -1,7 +0,0 @@
-email=$(git config user.email)
-
-if [ -z "$email" ]; then
-  echo "NOTE: git email is not configured. Set it with:"
-  echo "$ git config --system user.email deluan@deluan.com"
-  echo
-fi
